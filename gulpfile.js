8481bb54c40dd26f1473510212c0dc8984eb8ca1
maxjdev <maxjramosdev@gmail.com>
2023-12-07 13:10:49 -0300

    add: minificações e instalações de plugins minificadores

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
+const htmlmin = require('gulp-htmlmin');
 
 function styles() {
     return gulp.src('./src/styles/*.scss')
@@ -12,10 +14,27 @@ function styles() {
 function images() {
     return gulp.src('./src/images/**/*')
         .pipe(imagemin())
-        .pipe(gulp.dest('./dist/images'))
+        .pipe(gulp.dest('./dist/imgs'))
 }
 
-exports.default = gulp.parallel(styles, images);
+function scripts() {
+    return gulp.src('./src/scripts/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/js'))
+}
+
+function htmlmini () {
+    return gulp.src('./src/index.html')
+    .pipe(htmlmin({
+        collapseWhitespace: true,
+        collapseInlineTagWhitespace: true
+    }))
+    .pipe(gulp.dest('./dist'))
+}
+
+exports.default = gulp.parallel(styles, images, scripts, htmlmini);
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts))
+    gulp.watch('./src/*.html', gulp.parallel(htmlmini))
 }
